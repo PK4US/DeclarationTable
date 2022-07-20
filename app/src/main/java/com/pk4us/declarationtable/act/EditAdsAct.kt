370ab807dd41c4ca691823e7d49b51d9e9a5839f
PK4US <stepanchuk.ei@gmail.com>
2022-07-21 00:13:27 +0300

    Выбор и редактирование одного фото.

--- app/src/main/java/com/pk4us/declarationtable/act/EditAdsAct.kt
+++ app/src/main/java/com/pk4us/declarationtable/act/EditAdsAct.kt
@@ -58,7 +58,9 @@ class EditAdsAct : AppCompatActivity(),FragmentCloseInterface {
                 val returnValues = data.getStringArrayListExtra(Pix.IMAGE_RESULTS) as ArrayList<String>
                 if (returnValues.size > 1 && chooseImageFragment == null) {
                     openChooseImageFragment(returnValues)
-                } else if (chooseImageFragment != null) {
+                } else if (returnValues.size == 1 && chooseImageFragment == null) {
+                    imageAdapter.update(returnValues)
+                }else if (chooseImageFragment != null) {
                     chooseImageFragment?.updateAdapter(returnValues)
                 }
             }
