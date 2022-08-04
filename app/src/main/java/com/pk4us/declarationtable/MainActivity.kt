2748e98263aef74a904ffa70ab7268c20ab57024
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-04 14:23:31 +0300

    Получение фильтра и пути фильтра

--- app/src/main/java/com/pk4us/declarationtable/MainActivity.kt
+++ app/src/main/java/com/pk4us/declarationtable/MainActivity.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
