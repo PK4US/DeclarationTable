88d93dd717017376174294cf2ba99958fb12ac86
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-05 22:42:23 +0300

    Фильтр в категории Разное

--- app/src/main/java/com/pk4us/declarationtable/model/DbManager.kt
+++ app/src/main/java/com/pk4us/declarationtable/model/DbManager.kt
@@ -91,9 +91,21 @@ class DbManager {
            .startAt(filter).endAt(filter + "\uf8ff").limitToLast(ADS_LIMIT)
     }
 
-    fun getAllAdsNextPage(time:String,readDataCallback: ReadDataCallback?){
-        val query = db.orderByChild("/adFilter/time").endBefore(time).limitToLast(ADS_LIMIT)
-        readDataFromDb(query,readDataCallback)
+    fun getAllAdsNextPage(time:String,filter: String, readDataCallback: ReadDataCallback?){
+        if (filter.isEmpty()){
+            val query = db.orderByChild("/adFilter/time").endBefore(time).limitToLast(ADS_LIMIT)
+            readDataFromDb(query,readDataCallback)
+        }else{
+           getAllAdsByFilterNextPage(filter,time,readDataCallback)
+        }
+    }
+
+    private fun getAllAdsByFilterNextPage(tempFilter:String, time:String, readDataCallback: ReadDataCallback?){
+        val orderBy = tempFilter.split("|")[0]
+        val filter = tempFilter.split("|")[1]
+        val query = db.orderByChild("/adFilter/$orderBy")
+            .endBefore(filter + "_$time").limitToLast(ADS_LIMIT)
+        readNextPageFromDb(query,filter,orderBy,readDataCallback)
     }
 
     fun getAllAdsFromCatFirstPage(cat:String,filter: String, readDataCallback: ReadDataCallback?){
@@ -137,6 +149,34 @@ class DbManager {
                     ad?.emailCounter = infoItem?.emailCounter ?: "0"
                     ad?.callsCounter = infoItem?.callsCounter ?: "0"
                     if (ad!=null)adArray.add(ad!!)
+                }
+                readDataCallback?.readData(adArray)
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    private fun readNextPageFromDb(query: Query, filter: String,orderBy:String, readDataCallback: ReadDataCallback?){
+        query.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val adArray = ArrayList<Ad>()
+                for (item in snapshot.children){
+                    var ad:Ad? = null
+                    item.children.forEach {
+                        if (ad == null) ad = it.child(AD_NODE).getValue(Ad::class.java)
+                    }
+                    val infoItem = item.child(INFO_NODE).getValue(InfoItem::class.java)
+                    val filterNodeValue = item.child(FILTER_NODE).child(orderBy).value.toString()
+
+                    val favCounter = item.child(FAVS_MODE).childrenCount
+                    val isFav = auth.uid?.let { item.child(FAVS_MODE).child(it).getValue(String::class.java) }
+                    ad?.isFav = isFav !=null
+                    ad?.favCounter = favCounter.toString()
+
+                    ad?.viewsCounter = infoItem?.viewCounter ?: "0"
+                    ad?.emailCounter = infoItem?.emailCounter ?: "0"
+                    ad?.callsCounter = infoItem?.callsCounter ?: "0"
+                    if (ad!=null && filterNodeValue.startsWith(filter))adArray.add(ad!!)
                 }
                 readDataCallback?.readData(adArray)
             }
