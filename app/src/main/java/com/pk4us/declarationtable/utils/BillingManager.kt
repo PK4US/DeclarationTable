9c9e158437c66eff5984b3bc36acead060a7ec30
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-06 14:17:18 +0300

    Встроенные покупки #2

--- app/src/main/java/com/pk4us/declarationtable/utils/BillingManager.kt
+++ app/src/main/java/com/pk4us/declarationtable/utils/BillingManager.kt
@@ -1,18 +1,46 @@
 package com.pk4us.declarationtable.utils
 
 import androidx.appcompat.app.AppCompatActivity
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.*
 
 class BillingManager(val act:AppCompatActivity) {
     private var billingClient:BillingClient? = null
 
     init {
-
+        setUpBillingClient()
     }
 
     private fun setUpBillingClient(){
         billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener()).enablePendingPurchases().build()
+    }
+
+    fun startConnection(){
+        billingClient?.startConnection(object : BillingClientStateListener{
+            override fun onBillingServiceDisconnected() {
+
+            }
+
+            override fun onBillingSetupFinished(p0: BillingResult) {
+                getItem()
+            }
+        })
+    }
+
+    private fun getItem() {
+        val skuList = ArrayList<String>()
+        skuList.add(REMOVE_ADS)
+        val skuDetails = SkuDetailsParams.newBuilder()
+        skuDetails.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
+        billingClient?.querySkuDetailsAsync(skuDetails.build()) { result, list ->
+            run {
+                if (result.responseCode == BillingClient.BillingResponseCode.OK) {
+                    if (!list.isNullOrEmpty()){
+                        val billingFlowParams = BillingFlowParams.newBuilder().setSkuDetails(list[0]).build()
+                        billingClient?.launchBillingFlow(act,billingFlowParams)
+                    }
+                }
+            }
+        }
     }
 
     private fun getPurchaseListener():PurchasesUpdatedListener{
@@ -22,6 +50,9 @@ class BillingManager(val act:AppCompatActivity) {
                     list?.get(0).let {  }
             }
         }
+    }
 
+    companion object{
+        const val REMOVE_ADS = "remove_ads"
     }
 }
