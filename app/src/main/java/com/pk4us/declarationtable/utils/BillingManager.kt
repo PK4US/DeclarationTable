3c1bebde0731e80374e14932d127efbfd8693eb5
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-06 14:03:01 +0300

    Встроенные покупки #1

---
+++ app/src/main/java/com/pk4us/declarationtable/utils/BillingManager.kt
@@ -0,0 +1,27 @@
+package com.pk4us.declarationtable.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.PurchasesUpdatedListener
+
+class BillingManager(val act:AppCompatActivity) {
+    private var billingClient:BillingClient? = null
+
+    init {
+
+    }
+
+    private fun setUpBillingClient(){
+        billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener()).enablePendingPurchases().build()
+    }
+
+    private fun getPurchaseListener():PurchasesUpdatedListener{
+        return PurchasesUpdatedListener {
+                result, list ->
+            if (result.responseCode == BillingClient.BillingResponseCode.OK){
+                    list?.get(0).let {  }
+            }
+        }
+
+    }
+}
