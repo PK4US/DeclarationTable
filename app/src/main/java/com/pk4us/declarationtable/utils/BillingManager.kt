00012e7ee47dbee850d72c502a75cc31f27ac457
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-06 14:30:10 +0300

    Встроенные покупки #3

--- app/src/main/java/com/pk4us/declarationtable/utils/BillingManager.kt
+++ app/src/main/java/com/pk4us/declarationtable/utils/BillingManager.kt
@@ -1,5 +1,7 @@
 package com.pk4us.declarationtable.utils
 
+import android.content.Context
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.billingclient.api.*
 
@@ -12,6 +14,13 @@ class BillingManager(val act:AppCompatActivity) {
 
     private fun setUpBillingClient(){
         billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener()).enablePendingPurchases().build()
+    }
+
+    private fun savePurchase(isPurchase: Boolean){
+        val pref = act.getSharedPreferences(MAIN_PREF,Context.MODE_PRIVATE)
+        val editor = pref.edit()
+        editor.putBoolean(REMOVE_ADS_PREF,isPurchase)
+        editor.apply()
     }
 
     fun startConnection(){
@@ -43,16 +52,35 @@ class BillingManager(val act:AppCompatActivity) {
         }
     }
 
+    private fun nonConsumableItem(purchase: Purchase){
+        if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED){
+            if (purchase.isAcknowledged){
+                val acParams = AcknowledgePurchaseParams.newBuilder().setPurchaseToken(purchase.purchaseToken).build()
+                billingClient?.acknowledgePurchase(acParams){result->
+                    if (result.responseCode == BillingClient.BillingResponseCode.OK){
+                        savePurchase(true)
+                        Toast.makeText(act,"Реклама отключена!",Toast.LENGTH_SHORT).show()
+                    }else{
+                        savePurchase(false)
+                        Toast.makeText(act,"Ошибка! Не удалось реализовать покупку!",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
     private fun getPurchaseListener():PurchasesUpdatedListener{
         return PurchasesUpdatedListener {
                 result, list ->
             if (result.responseCode == BillingClient.BillingResponseCode.OK){
-                    list?.get(0).let {  }
+                    list?.get(0)?.let {nonConsumableItem(it)  }
             }
         }
     }
 
     companion object{
+        const val REMOVE_ADS_PREF = "remove_ads_pref"
         const val REMOVE_ADS = "remove_ads"
+        const val MAIN_PREF = "main_pref"
     }
 }
